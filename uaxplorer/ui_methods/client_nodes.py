adc1296718a0ff1d767df15b2aa926227121276f
FrictionalUnemployment <62852896+FrictionalUnemployment@users.noreply.github.com>
2021-02-21 21:27:32 +0100

    Changes to the Linking which is now recursive (In testing), mapping has been completely revamped to be instead clickable to navigate through (You double click to check the next variables in that node)

--- uaxplorer/ui_methods/client_nodes.py
+++ uaxplorer/ui_methods/client_nodes.py
@@ -3,7 +3,7 @@ from opcua import Client, ua
 from navigating_nodes import Navigating_nodes as navNodes
 
 class StandardItem(Qt.QStandardItem):
-    def __init__(self, txt='', font_size=10, set_bold=False, color=QtGui.QColor(255, 255, 255)):
+    def __init__(self, txt='', font_size=10, check_box=False, set_bold=False, color=QtGui.QColor(255, 255, 255)):
         super().__init__()
         self.setEditable(False)
         self.setForeground(color)
@@ -12,6 +12,7 @@ class StandardItem(Qt.QStandardItem):
         fnt = QtGui.QFont('Open Sans', font_size)
         fnt.setBold(set_bold)
         self.setFont(fnt)
+        self.setCheckable(check_box)
 
 
 class Client_nodes:
@@ -20,24 +21,24 @@ class Client_nodes:
         self.Server = server
         self.client = Client("opc.tcp://" + server)
 
-        self.ROOT_NODE = StandardItem(self.server_name, 10, True)
+        self.ROOT_NODE = StandardItem(self.server_name, 10, True, True)
         self.MAP_VALUE_NODES = {}
         self.FOLDER_NODE = []
-
-        NODE_MAP = {}
+        self.NODE_MAP = {}
+        self.NODE_ID = None
         nav_nodes = navNodes(self.client)
         try:
             self.client.connect()
-            NODE_MAP.update(nav_nodes.get_name_from_nodes(nav_nodes.get_children_nodes(nav_nodes.get_root_nodes())))
+            self.NODE_ID = nav_nodes.get_root_nodes()
+            self.NODE_MAP.update(nav_nodes.get_name_from_nodes(nav_nodes.get_children_nodes(nav_nodes.get_root_nodes())))
         finally:
             self.client.disconnect()
 
-        for key, values in NODE_MAP.items():
+        for key, values in self.NODE_MAP.items():
             for value in values:
                 if key not in self.MAP_VALUE_NODES:
                     self.MAP_VALUE_NODES[key] = list()
                 self.MAP_VALUE_NODES[key].append(StandardItem(value, 8, color=QtGui.QColor(180, 180, 180)))
 
         for key in self.MAP_VALUE_NODES:
-            print(key)
             self.FOLDER_NODE.append(StandardItem(key, 9, color=QtGui.QColor(200, 200, 200)))
