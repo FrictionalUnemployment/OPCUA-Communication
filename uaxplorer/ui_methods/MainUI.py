60491cf259c37c1087700f6171aa9588dd806a0b
FrictionalUnemployment <62852896+FrictionalUnemployment@users.noreply.github.com>
2021-03-03 19:01:17 +0100

    Added text

--- uaxplorer/ui_methods/MainUI.py
+++ uaxplorer/ui_methods/MainUI.py
@@ -211,7 +211,7 @@ class Ui_MainWindow(object):
                                         print(function_name)
                                         if('Methods' == function_name):
                                             bl = server1.client.get_node(i).call_method("2:subscribe","opc.tcp://" + server2.Server, str(dict1_key), str(key2))
-                                            print(bl, "Hi")
+                                            self.textBrowser.append(bl)
                                     server1.client.disconnect()
                                 if 'qx' in value and 'ix' in value2:
                                     server2.client.connect()
@@ -221,6 +221,7 @@ class Ui_MainWindow(object):
                                         
                                         if('Methods' == function_name):    
                                             bl = server1.client.get_node(i).call_method("2:subscribe","opc.tcp://" + server1.Server, str(key2), str(dict1_key))
+                                            self.textBrowser.append(bl)
                                     server2.client.disconnect()
         
         #server1.client.connect()
